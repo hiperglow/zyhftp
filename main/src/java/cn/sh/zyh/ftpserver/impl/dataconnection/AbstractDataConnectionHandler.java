5250d13f7cb84a58dd0117fe72c8167d0b4c1dcf
xuchangyuhui <xuchangyuhui@sohu.com>
2009-11-18 14:16:44 +0000

    refactor data connection.

--- main/src/java/cn/sh/zyh/ftpserver/impl/dataconnection/AbstractDataConnectionHandler.java
+++ main/src/java/cn/sh/zyh/ftpserver/impl/dataconnection/AbstractDataConnectionHandler.java
@@ -1,26 +1,6 @@
-///////////////////////////////////////////////////////////////////
-// Copyright:2009 by RICOH COMPANY, LTD. All Rights Reserved
-///////////////////////////////////////////////////////////////////
-/*
- * Copyright 2005 XJFTP Team (http://xjftp.sourceforge.net/)
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cn.sh.zyh.ftpserver.impl.dataconnection;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -29,176 +9,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cn.sh.zyh.ftpserver.api.Configuration;
-import cn.sh.zyh.ftpserver.impl.ConnectionHandler;
-import cn.sh.zyh.ftpserver.impl.core.CommandException;
-import cn.sh.zyh.ftpserver.impl.core.MessageKeys;
-import cn.sh.zyh.ftpserver.impl.core.Messages;
-import cn.sh.zyh.ftpserver.impl.representation.AsciiRepresentation;
-import cn.sh.zyh.ftpserver.impl.representation.Representation;
 import cn.sh.zyh.ftpserver.impl.utils.PortTracker;
 
-/**
- * I hold any common methods and member variables which will always be required
- * on a {@link cn.sh.zyh.ftpserver.impl.dataconnection.DataConnectionHandler}.
- * I can be sub-classes if you really need to override something at this level.
- * <p>
- * Copyright &copy; 2005 <a href="http://xjftp.sourceforge.net/">XJFTP Team</a>.
- * All rights reserved. Use is subject to <a
- * href="http://xjftp.sourceforge.net/LICENSE.TXT">licence terms</a> (<a
- * href="http://www.apache.org/licenses/LICENSE-2.0.html">Apache License v2.0</a>)<br/>
- * <p>
- * Last modified: $Date: 2005/05/22 23:17:38 $, by $Author: mmcnamee $
- * <p>
- * 
- * @author Mark McNamee (<a
- *         href="mailto:mmcnamee@users.sourceforge.net">mmcnamee at
- *         users.sourceforge.net</a>)
- * @version $Revision: 1.6 $
- */
 public abstract class AbstractDataConnectionHandler {
-	/**
-	 * Class name.
-	 */
+	/** Class name. */
 	private static final String CLASS_NAME = "AbstractDataConnectionHandler";
 
-	/** The ServerPI that uses this DTP. */
-	protected ConnectionHandler controlHandler;
-
-	/** The host of the data socket. */
-	protected String dataHost;
-
-	/** The port of the data socket. */
-	protected int dataPort = -1;
+	/** The passive server socket. */
+	private ServerSocket passiveServerSocket;
 
 	/** The passive server socket. */
-	protected ServerSocket passiveServerSocket;
-
-	/** The data socket. */
 	protected Socket dataSocket;
 
-	/** The client writer. */
-	protected OutputStream fos = null;
-
-	/** The client in stream. */
-	protected InputStream fis = null;
-
-	/** The flag of passive mode. */
-	protected boolean passive = false;
-
 	/** The flag of listening in passive mode. */
-	protected boolean listening = false;
+	private boolean listening = false;
 
-	/**
-	 * The representation being used for transmission. The initial
-	 * representation type is ASCII.
-	 */
-	protected Representation representation = AsciiRepresentation.getInstance();
-
-	/**
-	 * The debug logger instance.
-	 */
+	/** The debug logger instance. */
 	private final Logger logger = LoggerFactory.getLogger(CLASS_NAME);
-
-	/**
-	 * Sets the representation type used for transmission.
-	 * 
-	 * @param representation
-	 *            the representation
-	 */
-	public void setRepresentation(final Representation representation) {
-		this.representation = representation;
-	}
-
-	/**
-	 * Sets the data port for an active transmission.
-	 * 
-	 * @param port
-	 *            the port number to connect to.
-	 * @param host
-	 *            the host name to connect to.
-	 */
-	public void setDataPort(final String host, final int port) {
-		this.dataHost = host;
-		this.dataPort = port;
-	}
-
-	/**
-	 * Sets the flag of passive mode to be true.
-	 * 
-	 * @param isPassive
-	 *            the flag of passive mode
-	 */
-	public void setPassive(final boolean isPassive) {
-		this.passive = isPassive;
-	}
-
-	/**
-	 * Listen in passive mode.
-	 * 
-	 * @return the int
-	 * 
-	 * @throws IOException
-	 *             the IO exception
-	 */
-	public int listen() throws IOException {
-		final int port = assignPassiveListeningPort();
-
-		if (logger.isDebugEnabled()) {
-			logger.debug(CLASS_NAME, "listen",
-					"Creating new ServerSocket on port " + port);
-		}
-
-		this.passiveServerSocket = new ServerSocket(port);
-		this.passiveServerSocket.setSoTimeout(Configuration.TIME_OUT * 1000);
-		this.listening = true;
-		return port;
-	}
-
-	/**
-	 * Terminate client input stream.
-	 */
-	protected void terminateInputStream() {
-		if (this.fis != null) {
-			try {
-				if (this.dataSocket != null && !this.dataSocket.isClosed()) {
-					this.dataSocket.shutdownInput();
-				}
-				this.fis.close();
-			} catch (final IOException e) {
-				logger.warn(CLASS_NAME, "terminateClientInStream",
-						"Exception in close connection");
-			}
-		}
-	}
-
-	/**
-	 * Terminate passive server socket.
-	 */
-	protected void terminatePassiveServerSocket() {
-		if (this.passiveServerSocket != null) {
-			if (this.passiveServerSocket.isBound()
-					|| !this.passiveServerSocket.isClosed()) {
-				try {
-					this.passiveServerSocket.close();
-				} catch (final IOException e) {
-					logger.warn(CLASS_NAME, "terminatePassiveServerSocket",
-							"Exception in close connection");
-				}
-			}
-			this.passiveServerSocket = null;
-		}
-	}
 
 	/**
 	 * wait for client's connection.
 	 */
-	public void waitForClient() throws IOException {
+	public Socket waitForClient() throws IOException {
 		if (logger.isDebugEnabled()) {
-			logger.debug(CLASS_NAME, "connectToUser",
-					"In passive mode, awaiting data connection from client");
+			logger.debug("Passive mode, awaiting data connection from client");
 		}
 
-		this.dataSocket = this.passiveServerSocket.accept();
+		final int port = assignPassiveListeningPort();
+		final ServerSocket passiveServerSocket = new ServerSocket(port);
+		passiveServerSocket.setSoTimeout(Configuration.TIME_OUT * 1000);
+		listening = true;
+
+		dataSocket = passiveServerSocket.accept();
+		dataSocket.setKeepAlive(true);
+		dataSocket.setSoTimeout(Configuration.TIME_OUT * 1000);
 
 		if (logger.isDebugEnabled()) {
 			final StringBuffer sb = new StringBuffer("C:");
@@ -212,15 +56,12 @@ public abstract class AbstractDataConnectionHandler {
 			logger.debug("New Passive Data Connection from " + sb.toString());
 		}
 
-		dataSocket.setKeepAlive(true);
-		dataSocket.setSoTimeout(Configuration.TIME_OUT * 1000);
+		return dataSocket;
 	}
 
 	/**
 	 * Connect to client.
 	 * 
-	 * @throws CommandException
-	 *             the command exception
 	 * @throws IOException
 	 *             the IO exception
 	 */
@@ -233,11 +74,13 @@ public abstract class AbstractDataConnectionHandler {
 			return null;
 		}
 
-		final Socket dataSocket = new Socket();
+		dataSocket = new Socket();
 		dataSocket.setReuseAddress(true);
 		dataSocket.connect(new InetSocketAddress(dataHost, dataPort));
 		dataSocket.setKeepAlive(true);
 		dataSocket.setSoTimeout(Configuration.TIME_OUT * 1000);
+
+		logger.debug("Client: ip " + dataHost + " port " + dataHost);
 
 		return dataSocket;
 	}
@@ -245,32 +88,33 @@ public abstract class AbstractDataConnectionHandler {
 	/**
 	 * Terminate data connection.
 	 */
-	protected void terminateDataConnection() {
-		if (this.dataSocket != null) {
-			if (this.dataSocket.isConnected()) {
+	protected void closeDataConnection() {
+		if (dataSocket != null) {
+			if (dataSocket.isConnected()) {
 				try {
-					this.dataSocket.close();
+					dataSocket.close();
 				} catch (final IOException e) {
-					logger.warn(CLASS_NAME, "terminateDataConnection",
-							"Exception in close connection");
+					logger.warn("IOException in close dataSocket.");
 				}
 			}
-			this.dataSocket = null;
-			terminatePassiveServerSocket();
+			dataSocket = null;
 		}
 	}
 
 	/**
-	 * Terminate client writer.
+	 * Terminate passive server socket.
 	 */
-	protected void terminateOutputStream() {
-		if (this.fos != null) {
-			try {
-				fos.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+	protected void closePassiveServerSocket() {
+		if (this.passiveServerSocket != null) {
+			if (this.passiveServerSocket.isBound()
+					|| !this.passiveServerSocket.isClosed()) {
+				try {
+					this.passiveServerSocket.close();
+				} catch (final IOException e) {
+					logger.warn("IOException in close Passive ServerSocket.");
+				}
 			}
-			fos = null;
+			this.passiveServerSocket = null;
 		}
 	}
 
@@ -279,11 +123,11 @@ public abstract class AbstractDataConnectionHandler {
 	 * 
 	 * @return the int
 	 */
-	protected int assignPassiveListeningPort() {
+	private int assignPassiveListeningPort() {
 		// If for some reason the tidy up from the last connection failed,
 		// ensure it's done this time!
 		if (this.listening) {
-			terminatePassiveServerSocket();
+			closePassiveServerSocket();
 		}
 		return PortTracker.getInstance().getPort();
 	}
--- main/src/java/cn/sh/zyh/ftpserver/impl/dataconnection/DataConnectionHandler.java
+++ main/src/java/cn/sh/zyh/ftpserver/impl/dataconnection/DataConnectionHandler.java
@@ -1,136 +1,82 @@
-///////////////////////////////////////////////////////////////////
-// Copyright:2009 by RICOH COMPANY, LTD. All Rights Reserved
-///////////////////////////////////////////////////////////////////
-/*
- * Copyright 2005 XJFTP Team (http://xjftp.sourceforge.net/)
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cn.sh.zyh.ftpserver.impl.dataconnection;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.ConnectException;
-import java.net.Socket;
-import java.util.Iterator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import cn.sh.zyh.ftpserver.impl.command.list.FtpFile;
-import cn.sh.zyh.ftpserver.impl.core.CommandException;
+import cn.sh.zyh.ftpserver.api.Configuration;
 import cn.sh.zyh.ftpserver.impl.representation.Representation;
 
-/**
- * This is the server data transfer process. It is responsible for transferring
- * files to and from the client. A separate data socket is created to transfer
- * the data.
- * <p>
- * Copyright &copy; 2005 <a href="http://xjftp.sourceforge.net/">XJFTP Team</a>.
- * All rights reserved. Use is subject to <a
- * href="http://xjftp.sourceforge.net/LICENSE.TXT">licence terms</a> (<a
- * href="http://www.apache.org/licenses/LICENSE-2.0.html">Apache License v2.0</a>)<br/>
- * <p>
- * Last modified: $Date: 2005/05/22 23:17:37 $, by $Author: mmcnamee $
- * <p>
- * 
- * @author Mark McNamee (<a
- *         href="mailto:mmcnamee@users.sourceforge.net">mmcnamee at
- *         users.sourceforge.net</a>)
- * @version $Revision: 1.5 $
- */
 public class DataConnectionHandler extends AbstractDataConnectionHandler {
-	/**
-	 * Class name.
-	 */
+	/** Class name. */
 	private static final String CLASS_NAME = "DataConnectionHandler";
 
-	/**
-	 * The debug logger instance.
-	 */
+	/** The debug logger instance. */
 	private final Logger logger = LoggerFactory.getLogger(CLASS_NAME);
+
+	/** FileOutputStream used for writing received data to a local file! */
+	protected FileOutputStream fos = null;
+
+	/** FileInputStream used for reading local files to send to the client! */
+	protected FileInputStream fis = null;
 
 	/**
 	 * Opens the data connection, reads the data according to the current
 	 * transmission mode, representation type and structure, and writes it into
 	 * the local file "path".
 	 */
-	public boolean receiveFile(String path) throws CommandException {
+	public void receiveFile(final Representation representation, final File file)
+			throws IOException {
+		InputStream in = null;
 		try {
-			File file = new File(path);
-			if (file.exists()) {
-				// remove the old one - overwrite is ok!
-				// throw new CommandException(550, "File exists in that
-				// location.");
-				file.delete();
+			// File file = new File(path);
+			// if (file.exists()) {
+			// // remove the old one - overwrite is ok!
+			// throw new CommandException(550, "File exists in that location.");
+			// file.delete();
+			// }
+
+			fos = new FileOutputStream(file);
+
+			in = representation.getInputStream(dataSocket);
+			byte buf[] = new byte[Configuration.BUFFER_SIZE];
+			int nread;
+			while ((nread = in.read(buf, 0, 4096)) > 0) {
+				fos.write(buf, 0, nread);
 			}
+			in.close();
+		} // throw new CommandException(550, "Can't write to file");
+		finally {
+			closeInputStream(in);
+			closeOutputStream(fos);
+			closeDataConnection();
+			closePassiveServerSocket();
+		}
+	}
 
-			// connectToUser();
-
-			this.fos = new FileOutputStream(file);
-
-			// Read file contents.
-			//
-			// this.controlHandler.reply(150, "Opening "
-			// + this.representation.getName() + " mode data connection.");
-			receiveFile(this.dataSocket, this.fos, this.representation);
-			return true;
-		} catch (ConnectException e) {
-			throw new CommandException(425, "Can't open data connection.");
+	public void sendData(final Representation representation, final String data) {
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(representation.getOutputStream(dataSocket));
+			writer.write(data);
+			writer.flush();
 		} catch (IOException e) {
-			throw new CommandException(550, "Can't write to file");
+			logger.debug("IOException in send file list to client.");
 		} finally {
-			terminateOutputStream();
-			terminateDataConnection();
-			terminatePassiveServerSocket();
+			if (writer != null) {
+				writer.close();
+				writer = null;
+			}
+			closeDataConnection();
+			closePassiveServerSocket();
 		}
-	}
-
-	/**
-	 * Reads the contents of the file from "in", and writes the data to the
-	 * given socket using the specified representation.
-	 */
-	private void sendFile(InputStream in, Socket s,
-			Representation representation) throws IOException {
-		OutputStream out = representation.getOutputStream(s);
-		byte buf[] = new byte[4096];
-		int nread;
-		while ((nread = in.read(buf)) > 0) {
-			out.write(buf, 0, nread);
-		}
-		out.close();
-	}
-
-	/**
-	 * Reads data from the given socket and converts it from the specified
-	 * representation to local representation, writing the result to a file via
-	 * "out".
-	 */
-	private void receiveFile(Socket s, OutputStream out,
-			Representation representation) throws IOException {
-		InputStream in = representation.getInputStream(s);
-		byte buf[] = new byte[4096];
-		int nread;
-		while ((nread = in.read(buf, 0, 4096)) > 0) {
-			out.write(buf, 0, nread);
-		}
-		in.close();
 	}
 
 	/**
@@ -138,62 +84,63 @@ public class DataConnectionHandler extends AbstractDataConnectionHandler {
 	 * the data socket using the current transmission mode, representation type
 	 * and structure.
 	 */
-	public boolean sendFile(String path) throws CommandException {
+	public void sendFile(final Representation representation, final File file)
+			throws IOException {
+		OutputStream out = null;
 		try {
-			File file = new File(path);
-			if (!file.isFile()) {
-				throw new CommandException(550, "Not a plain file.");
+			fis = new FileInputStream(file);
+			out = representation.getOutputStream(dataSocket);
+			byte buf[] = new byte[Configuration.BUFFER_SIZE];
+			int nread;
+			while ((nread = fis.read(buf)) > 0) {
+				out.write(buf, 0, nread);
 			}
 
-			this.fis = new FileInputStream(file);
-
-			// connectToUser();
-
-			// Send file contents.
-			//
-			// this.controlHandler.reply(150, "Opening "
-			// + this.representation.getName() + " mode data connection.");
-			sendFile(this.fis, this.dataSocket, this.representation);
-			return true;
-		} catch (FileNotFoundException e) {
-			throw new CommandException(550, "No such file.");
-		} catch (ConnectException e) {
-			throw new CommandException(425, "Can't open data connection.");
-		} catch (IOException e) {
-			throw new CommandException(553, "Not a regular file.");
-		} finally {
-			terminateInputStream();
-			terminateDataConnection();
 		}
-	}
-
-	public void sendFiles(Socket dataSocket,
-			final Representation representation, final String filesInfo) {
-		try {
-			PrintWriter writer = new PrintWriter(representation
-					.getOutputStream(dataSocket));
-
-			writer.write(filesInfo);
-			writer.flush();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (dataSocket.isConnected()) {
-				try {
-					dataSocket.close();
-				} catch (final IOException e) {
-					logger.warn("Exception in close connection");
-				}
-			}
-			dataSocket = null;
+		// throw new CommandException(553, "Not a regular file.");
+		finally {
+			closeOutputStream(out);
+			closeInputStream(fis);
+			closeDataConnection();
 		}
 	}
 
 	public void abortTransfer() {
-		this.terminateInputStream();
-		this.terminateOutputStream();
-		this.terminateDataConnection();
-		this.terminatePassiveServerSocket();
+		this.closeInputStream(fis);
+		this.closeOutputStream(fos);
+		this.closeDataConnection();
+		this.closePassiveServerSocket();
+	}
+
+	/**
+	 * Terminate client input stream.
+	 */
+	private void closeInputStream(InputStream fis) {
+		if (fis != null) {
+			try {
+				if (dataSocket != null && !dataSocket.isClosed()) {
+					dataSocket.shutdownInput();
+				}
+				fis.close();
+				fis = null;
+			} catch (final IOException e) {
+				logger.warn("IOException in close InputStream.");
+			}
+		}
+	}
+
+	/**
+	 * Terminate client writer.
+	 */
+	private void closeOutputStream(OutputStream fos) {
+		if (fos != null) {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				logger.warn("IOException in close OutputStream.");
+			}
+			fos = null;
+		}
 	}
 
 }
